f94174c8cfc66ad098075d88c4afbcd0074df182
Marco Antonio Velázquez Velázquez <velazquezv.marco@gmail.com>
2023-07-03 10:42:02 -0600

    Solved 3

--- src/main/java/com/epam/rd/autotasks/LoopStatements.java
+++ src/main/java/com/epam/rd/autotasks/LoopStatements.java
@@ -1,16 +1,19 @@
 package com.epam.rd.autotasks;
 class LoopStatements {
     public static int sumOddDigits(int n) {
-        int sum = 0;
-        while (n > 0) {
-            int digit = n % 10; //Extract the last digit
-            if (digit % 2 != 0) {
-                sum += digit;
-            }
-            n /= 10;
+        if (n < 0) {
+            throw new IllegalArgumentException("n must be a positive number");
         }
-        return sum;
-    }
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10; //Extract the last digit
+                if (digit % 2 != 0) {
+                    sum += digit;
+                }
+                n /= 10;
+            }
+            return sum;
+        }
 
     public static void main(String[] args) {
         int number = 1234;
