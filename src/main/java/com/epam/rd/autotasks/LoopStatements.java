31b0937e4407ab289acb64d94d7da9be0cfea983
Marco Antonio Velázquez Velázquez <velazquezv.marco@gmail.com>
2023-07-03 10:24:56 -0600

    Solved

--- src/main/java/com/epam/rd/autotasks/LoopStatements.java
+++ src/main/java/com/epam/rd/autotasks/LoopStatements.java
@@ -1,13 +1,30 @@
 package com.epam.rd.autotasks;
-
-
 class LoopStatements {
     public static int sumOddDigits(int n) {
-        //TODO: Delete line below and write your own solution
-        throw new UnsupportedOperationException();
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10; //Extract the last digit
+            if (digit % 2 != 0) {
+                sum += digit;
+            }
+            n /= 10;
+        }
+        return sum;
     }
 
+    public static void main(String[] args) {
+        int number = 1234;
+        int sum = sumOddDigits(number);
+        System.out.println("Sum of odd digits: " + sum);
 
-
+        number = 246;
+        sum = sumOddDigits(number);
+        System.out.println("Sum of odd digits: " + sum);
+    }
 
 }
+
+
+
+
+
